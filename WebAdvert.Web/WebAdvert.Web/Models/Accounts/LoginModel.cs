847049b4a7b5e1a0db6f04f8b85697ede85d3237
Anil4.Yadav <Anil4.Yadav@LT0075972>
2020-06-05 17:13:46 +0530

    first login/signup page added and checked in

---
+++ WebAdvert.Web/WebAdvert.Web/Models/Accounts/LoginModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAdvert.Web.Models.Accounts
+{
+    public class LoginModel
+    {
+        [Required(ErrorMessage ="Please enter username")]
+        [Display(Name ="Email / Username")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Please enter password")]
+        [Display(Name ="Password")]
+        public string Password { get; set; }
+        public bool RememberMe { get; set; }
+    }
+}
